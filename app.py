3897ab60062a884e121b418746744da7aca7c871
RyanSyme <ryu321@hotmail.com>
2021-01-28 11:05:55 +0000

    create add sandwich form

--- app.py
+++ app.py
@@ -98,6 +98,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_sandwich")
+def add_sandwich():
+    return render_template("add_sandwich.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
